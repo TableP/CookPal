78007ba6af5d73bd3bdb0629cc716abd708cb184
Nduka Ofoeyeno <nofoeyeno@gmail.com>
2024-03-13 18:16:03 +0000

    front end up to date

--- CookPal/app/urls.py
+++ CookPal/app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', views.HomepageView.as_view(), name='homepage'),
     path('about/', views.AboutView.as_view(), name='about'),
     path('contact-us/', views.ContactUsView.as_view(), name='contactus'),
-    path('login/', views.LoginView.as_view(), name='login'),
+##    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('report/<reportid>/', views.ReportView.as_view(), name='report'),
 
